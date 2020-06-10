04c88d4703d4835fbee73d3206a18c6812b84fcd
it_layout@163.com <Cl15431898>
2020-06-11 04:12:00 +0800

    新增swagger文档

--- src/main/java/com/socketIO/demo/DTO/MessageDTO.java
+++ src/main/java/com/socketIO/demo/DTO/MessageDTO.java
@@ -1,15 +1,20 @@
 package com.socketIO.demo.DTO;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * 消息实体类
  * @param <T>
  */
 public class MessageDTO<T> {
 
+    @ApiModelProperty(value = "目标用户id", required = true)
     private String userId;
 
+    @ApiModelProperty(value = "消息名称", required = true)
     private String message;
 
+    @ApiModelProperty(value = "消息内容")
     private T data;
 
     public String getUserId() {
--- src/main/java/com/socketIO/demo/controller/IndexController.java
+++ src/main/java/com/socketIO/demo/controller/IndexController.java
@@ -2,8 +2,11 @@ package com.socketIO.demo.controller;
 
 import com.socketIO.demo.DTO.MessageDTO;
 import com.socketIO.demo.config.SocketIOHandle;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "消息推送中心")
 @RestController
 public class IndexController {
 
@@ -11,6 +14,7 @@ public class IndexController {
      * 向客户端发送消息
      * @param messageDTO
      */
+    @ApiOperation(value = "订阅消息")
     @RequestMapping(value = "/updateMsg", method = RequestMethod.POST)
     public void updateMsg(@RequestBody MessageDTO messageDTO) {
         SocketIOHandle.pushMessage(messageDTO);
